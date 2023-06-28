58f2f66b52e57b7e8b35f66a677b0e47145e2b34
luoyin <912271673@qq.com>
2023-06-28 14:22:23 +0800

    almost complete simpleList

--- clib/list/simpleList.c
+++ clib/list/simpleList.c
@@ -1,4 +1,5 @@
 #include "simpleList.h"
+#include <stdint.h>
 //init functions
 simpleList * simpleList_init_void(){
     simpleList* self = (simpleList*) malloc(sizeof(simpleList));
@@ -33,7 +34,7 @@ int32_t simpleList_append_int32_object(simpleList * self,object value){
     self->positionCounter++;
     return self->positionCounter - 1;
 }
-//TODO need rewrite "have"
+
 bool simpleList_have_bool_object(simpleList* self,bool equals(object,object),object value){
     bool have = false;
     for(int32_t i;i != self->positionCounter;i++){
@@ -42,13 +43,18 @@ bool simpleList_have_bool_object(simpleList* self,bool equals(object,object),obj
     return have;
 }
 
-int32_t simpleList_set_int32_int32_object(simpleList* self,int32_t position,object value){
-    if(position > self->valueCounter || position < 0){
+int32_t simpleList_set_int32_int32_object(simpleList* self,int32_t _position,object value){
+    int32_t position = _position;
+    if(position > self->positionCounter-1){
         return -(position);
     }
-    if(position<self->positionCounter){
-        free(self->objectArray[position]);
+    if(position < 0){
+	    if(-(position) > self->valueCounter+1){
+		    return -(position);
+	    }
+	    position = self->valueCounter + postion + 1;
     }
+    free(self->objectArray[position]);
     self->objectArray[position] = value;
     return position;
 }
@@ -91,17 +97,36 @@ void simpleList_close(simpleList* self){
     free(self->objectArray);
     free(self);
 }
-//TODO pop with something wrong
+
+extern object deleteButNotFree(simpleList*,int32_t);
+
 object simpleList_pop_object_void(simpleList* self){
-    object tmp = simpleList_get_object_void(self);
-    simpleList_delete_bool_int32(self,self->positionCounter-1);
+    object tmp = deleteButNotFree(self,self->positionCounter-1);
     return tmp;
 }
 
 object simpleList_pop_object_int32(simpleList* self,int32_t position){
-    object tmp = simpleList_get_object_int32(self,position);
-    simpleList_delete_bool_int32(self,position);
+    object tmp = deleteButNotFree(self,position);
     return tmp;
 }
-//TODO
+
+object deleteButNotFree(simpleList* self,int32_t position){
+	object* tmpPtr = malloc((self->valueCounter+1)*sizeof(object));
+	int32_t offSet = 0;
+	object resualt = NULL;
+	for(int32_t i = 0;i != self->positionCounter;i++){
+		if(i == position){
+			offSet++;
+			//free(self->objectArray[i])
+			resualt = self->objectArray[i];
+			continue;
+		}
+		tmpPtr[i-offSet] = self->objectArray[i];
+	}
+	free(self->objectArray);
+	self->objectArray = tmpPtr;
+	self->positionCounter--;
+	return resualt;
+}
+
 //end impl
